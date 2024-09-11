f40cdf055311505f0ebffa2c4660adcf810b140e
phil <philjke@mail.ru>
2024-09-11 11:38:20 +0700

    Quartz sync: Sep 11, 2024, 11:38 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Phil Obsidian",
+    pageTitle: "Phil Obsidian 🚀",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
